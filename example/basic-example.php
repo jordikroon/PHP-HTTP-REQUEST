d5ba1792c9e06c840481a544ecfc44bfc1cbddcd
Jordi <jordi@sficlan.com>
2012-12-03 16:14:28 +0100

    Version 1.1.1
    Bug fix, file clean

--- example/basic-example.php
+++ example/basic-example.php
@@ -5,15 +5,19 @@ include_once '../src/httprequest.class.php';
 $_POST['postvalue1'] = 'something';
 $_POST['postvalue2'] = 'something else';
 
-$request = new HTTPRequest('http://example.com/demo.php');
+$request = new HTTPRequest();
 
-$request->method = 'post'; 
-$request->postFields = $_POST;
+$request->setURL('http://example.com/demo.php');
 
-$request->userAgent = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:16.0) Gecko/20100101 Firefox/16.0';
+$request->method = 'POST'; //POST or GET
+$request->postFields = $_POST; // POST array or Associative array
+$request->forceOneConnection = false; // True = always 1 connection
+$request->requestType = 1; // 1: CURL 2: Content request
+
+$request->userAgent = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:16.0) Gecko/20100101 Firefox/16.0'; // useragent
 $request->timeout = 10; // timeout after 10 seconds 
-$request->cookiedir = 'tmp';
-
+$request->cookiedir = '/path/to/tmp'; // path to cookie storage folder
+ 
 if($request->send()) { 
 	echo $request->getResult();
 }  else {
